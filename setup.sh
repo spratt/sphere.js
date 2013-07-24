f3e81318a170aca534a65d4816727cdf44fba229
Simon Pratt <simon@pr4tt.com>
2013-07-24 15:21:59 -0400

    Incomplete setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd lib
+wget http://github.com/mrdoob/three.js/zipball/master
+# TODO: move three.js into the lib/ folder
+# NB: can't write/test on mac, since wget from https is broken
